f4eed53038cf57a3c08cf4ba1a5ad7c05dbc1228
blawness <yudhahafiz@gmail.com>
2026-06-23 14:58:02 +0700

    fix(ui): remove unused X import from Navbar

--- packages/ui/src/components/layout/Navbar.tsx
+++ packages/ui/src/components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Menu, X } from "lucide-react";
+import { Menu } from "lucide-react";
 import { cn } from "../../lib/utils.js";
 import { Button } from "../ui/button.js";
 
